bc7c78bb7d0068a0c02246c4db8e158484767001
mkesyasakha <137471717+mkesyasakha@users.noreply.github.com>
2024-08-01 22:30:48 +0700

    Spatie Installation

---
+++ database/seeders/RoleAndPermissionSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
+class RoleAndPermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Role::create(['name' => 'super_admin']);
+        Role::create(['name' => 'admin']);
+        Role::create(['name' => 'guest']);
+    }
+}
