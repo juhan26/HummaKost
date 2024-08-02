9698729d093a24761d171ff923c8765140c0fc65
iamCelott <Dcviriya313@gmail.com>
2024-08-02 18:51:37 +0800

    add has paid until at financials

--- database/migrations/2024_08_02_023434_create_financials_table.php
+++ database/migrations/2024_08_02_023434_create_financials_table.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration
 {
     /**
-     * Run the migrations.
+     * Run the migravtions.
      */
     public function up(): void
     {
