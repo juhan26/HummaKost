648ea77289252e55c65daf6d218cfc6d70fc7a96
ridoq <ridoq444@gmail.com>
2024-08-05 14:50:33 +0700

    ubah konsep

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,7 +18,6 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RoleAndPermissionSeeder::class,
             UserSeeder::class,
-            DormInformationSeeder::class,
            ]);
     }
 }
