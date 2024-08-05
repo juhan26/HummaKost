ad249d12d5a0cc640249446efb2534d05c8f8c9a
iamCelott <Dcviriya313@gmail.com>
2024-08-06 07:47:25 +0800

    create furniture -mcr

---
+++ app/Http/Controllers/FurnitureController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreFurnitureRequest;
+use App\Http\Requests\UpdateFurnitureRequest;
+use App\Models\Furniture;
+use Illuminate\Http\Request;
+
+class FurnitureController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $furnitures = Furniture::all();
+        return view('pages.furnitures.index', compact('furnitures'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreFurnitureRequest $request)
+    {
+        Furniture::create($request->all());
+        return redirect()->route('furniture.index')->with('success', "Successful Created Furniture");
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Furniture $furniture)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Furniture $furniture)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Furniture $furniture)
+    {
+        $furniture->update($request->all());
+        return redirect()->route('furniture.update')->with('success', "Successful Updated Furniture");
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Furniture $furniture)
+    {
+        $furniture->delete();
+        return redirect()->route('furniture.update')->with('success', "Successful Deleted Furniture");
+    }
+}
