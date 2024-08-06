237fd356e705a0107506292ccffc8778e87aac25
iamCelott <Dcviriya313@gmail.com>
2024-08-06 08:35:19 +0800

    create furniture route resource and page

--- routes/chandra.php
+++ routes/chandra.php
@@ -1,5 +1,9 @@
 <?php
+
+use App\Http\Controllers\FurnitureController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
-
-
+Route::middleware(['auth', 'role:super_admin'])->group(function () {
+    Route::resource('furnitures', FurnitureController::class);
+});
