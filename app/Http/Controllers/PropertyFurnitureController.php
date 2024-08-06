bb5dfb6b9258a2f7b2719cd69a9dacc093f91a94
mkesyasakha <137471717+mkesyasakha@users.noreply.github.com>
2024-08-06 21:57:29 +0700

    Add View Leases

--- app/Http/Controllers/PropertyFurnitureController.php
+++ app/Http/Controllers/PropertyFurnitureController.php
@@ -17,7 +17,7 @@ class PropertyFurnitureController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
+     * Show the form for creating a new resource.-
      */
     public function create()
     {
