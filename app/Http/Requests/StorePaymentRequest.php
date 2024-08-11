6756a87dd0c90ed5a83a2b4ed81745b78ea27d4b
mkesyasakha <137471717+mkesyasakha@users.noreply.github.com>
2024-08-12 00:00:30 +0700

    Added Payments Per Month

---
+++ app/Http/Requests/StorePaymentRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StorePaymentRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'lease_id' => ['required', 'exists:leases,id'],
+            'month' => ['required', 'date', 'after_or_equal:today'],
+            // 'nominal' => ['required', 'numeric', 'max:255'],
+            'description' => ['nullable', 'string', 'max:255'],
+        ];
+    }
+}
---
+++ database/migrations/2024_08_11_095347_create_payment_per_months_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payment_per_months', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('lease_id')->constrained()->onDelete('restrict')->onUpdate('cascade');
+            $table->date('month');
+            $table->decimal('nominal', 10, 2);
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payment_per_months');
+    }
+};
