588e7a7768e0a59af5839977ac50e55ec42955ec
iamCelott <Dcviriya313@gmail.com>
2024-08-21 17:31:32 +0800

    update leases ui and add the filter

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -54,7 +54,7 @@ class UserController extends Controller
         END
     ")
             ->latest()
-            ->paginate(10);
+            ->paginate(1);
 
         $instances = Instance::orderBy('name', 'ASC')->get();
 
