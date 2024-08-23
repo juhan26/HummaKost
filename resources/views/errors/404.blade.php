f91db1c9a797bd1de63d353f18732a5441bd2efe
Andhika Mugni <165865178+Akusehatsekaliya@users.noreply.github.com>
2024-08-23 13:05:09 +0700

    ui errors 404 403

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -6,17 +6,16 @@
     <title>Halaman Tidak Ditemukan</title>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 </head>
-<body class="bg-gradient-to-r from-blue-600 via-teal-500 to-green-500 flex items-center justify-center h-screen">
-    <div class="text-center bg-white p-12 rounded-xl shadow-2xl transform transition-all duration-500 hover:scale-105 hover:shadow-3xl">
-        <div class="mb-6">
-            <svg class="mx-auto w-28 h-28 text-blue-600" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M9 14l2-2m0 0l2-2m-2 2l-2-2m2 2v6m-5 4h10a2 2 0 002-2v-5a2 2 0 00-2-2H7a2 2 0 00-2 2v5a2 2 0 002 2zm0 0v-5a2 2 0 012-2h6a2 2 0 012 2v5"></path>
-            </svg>
+<body class="bg-white flex items-center justify-center h-screen">
+    <div class="text-center mx-4 sm:mx-8 lg:mx-0">
+        <!-- Gambar 404 -->
+        <div class="relative inline-block">
+            <img src="{{ asset('images/404.png') }}" alt="404 Image" class="mx-auto" width="658.3px" height="298.81">
         </div>
-        <h1 class="text-8xl font-black text-gray-800 mb-4">404</h1>
-        <p class="text-3xl text-gray-600 mb-4">Oops! Halaman Tidak Ditemukan</p>
-        <p class="text-lg text-gray-500 mb-8">Maaf, halaman yang Anda cari mungkin telah dipindahkan atau tidak lagi tersedia.</p>
-        <a href="/" class="inline-block bg-green-600 text-white px-10 py-4 rounded-full shadow-lg hover:bg-green-700 transition duration-300 ease-in-out transform hover:scale-110">Kembali ke Beranda</a>
+
+        <!-- Pesan Error -->
+        <h1 class="text-3xl font-bold text-gray-800 mb-2" style="color: #20B486;">HALAMAN TIDAK DITEMUKAN</h1>
+        <a href="/" class="mt-4 inline-block text-white px-8 py-3 rounded-full shadow-lg" style="background-color: #20B486">Kembali ke Beranda</a>
     </div>
 </body>
 </html>
