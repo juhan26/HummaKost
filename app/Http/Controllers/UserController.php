396800bf417f237bd9bfdc878591b14e735bf5f8
iamCelott <Dcviriya313@gmail.com>
2024-08-27 13:02:08 +0800

    update user edit callnumber bug

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -178,7 +178,7 @@ class UserController extends Controller
         }
 
         $user->name = $request->input('name');
-        $user->phone_number = $request->input('phone_number');
+        $user->phone_number = $request->phone_number;
 
         if ($request->memberMenu === "y") {
             $user->gender = $request->input('gender');
