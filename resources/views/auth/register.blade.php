88940beb68a446fa2fe6e2ed5eb52fbc60464975
ridoq <ridoq444@gmail.com>
2024-08-29 13:18:58 +0700

    filter icon

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -31,6 +31,9 @@
     <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&amp;ampdisplay=swap"
         rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
     <link rel="stylesheet" href="../../assets/vendor/fonts/remixicon/remixicon.css" />
     <link rel="stylesheet" href="../../assets/vendor/fonts/flag-icons.css" />
 
@@ -127,8 +130,8 @@
             <!--  Multi Steps Registration -->
             <div class="d-flex col-lg-8 align-items-center justify-content-center authentication-bg p-5"
                 style="position: relative">
-                <a href="/" style="position: absolute;top:20px;left:20px;"><span class="material-symbols-outlined"
-                        style="font-size: 40px">
+                <a href="/" style="position: absolute;top:20px;left:20px;"><span
+                        class="material-symbols-outlined" style="font-size: 40px">
                         arrow_back
                     </span></a>
                 <div class="w-px-700 mt-12 mt-lg-0 pt-lg-0 pt-4">
