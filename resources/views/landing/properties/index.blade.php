ac06e930b7f2b2e505e029f8f4eb3fc7266ee1dd
ridoq <ridoq444@gmail.com>
2024-08-29 10:26:18 +0700

    payment card

--- resources/views/landing/properties/index.blade.php
+++ resources/views/landing/properties/index.blade.php
@@ -12,7 +12,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
-    <link href="https://cdn.jsdelivr.net/npm/remixicon@4.3.0/fonts/remixicon.css" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.n          et/npm/remixicon@4.3.0/fonts/remixicon.css" rel="stylesheet" />
 
     <!-- favicon -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/vanilla-tilt/1.7.0/vanilla-tilt.min.js"></script>
